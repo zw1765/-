1be57b335380140346aa3892188f29f5856c5702
zw1765 <1765127077@qq.com>
2021-08-26 20:57:26 +0800

    数据库更新

--- xpath解析链家深圳二手房.py
+++ xpath解析链家深圳二手房.py
@@ -9,6 +9,7 @@ Created on Thu Aug 26 14:57:17 2021
 import pandas as pd
 import requests
 from lxml import etree
+import pymysql
 
 # 模拟浏览器: 让服务器认为我是在用浏览器访问他的数据
 headers = {
@@ -44,10 +45,44 @@ def get_house(page=1):
 def Save_csv(data,name):
     df=pd.DataFrame.from_dict(data)
     df.to_csv(f'./data/{name}.txt')
+
+def save_sql(data,name):
+    df=pd.DataFrame(data)
+    db=pymysql.connect(host='localhost',
+                       user='root',
+                       password='Uplooking_123',
+                       port=3306,
+                       db='spiders',
+                       charset='utf8')
     
+    cursor=db.cursor()
+    cursor.execute(f'drop table if exists {name}_post')
+    
+    sql=f'''create table {name}_post(
+    title varchar(256)  not null,
+    positionInfo varchar(256) not null,
+    Total_price varchar(256) not null,
+    Unit_price varchar(256) not null
+    
+   )'''
+    
+    cursor.execute(sql)
+    db.commit()
         
+    with db.cursor() as cur:
+        for i in range(len(df)):
+            ds=df.iloc[i].values
+            cur.execute(f'''
+                        insert into {name}_post(title, positionInfo, Total_price, Unit_price )  
+                        values({ds[0]},'{ds[1]}',
+                                '{ds[2]}','{ds[3]}')
+                        ''')
+            db.commit()
+            
+    
 # 标题  小区  总价  单价   存储到txt文件中
 def main():
+    
     data=get_house()
     
     Save_csv(data, 'fist')
